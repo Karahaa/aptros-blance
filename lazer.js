f3ed8fe5545b36c1b61da5a8618975b123845903
Ialalendd <71377056+Ialalendd@users.noreply.github.com>
2024-12-15 19:06:13 +0000

    Create lazer.js

---
+++ lazer.js
@@ -0,0 +1,34 @@
+const headers = [
+    { id: 'n', title: '№' },
+    { id: 'Wallet', title: 'Wallet' },
+    { id: 'Clusters', title: 'Clusters' },
+    { id: 'TX Count', title: 'TX Count' },
+    { id: 'Source chains', title: 'Source chains' },
+    { id: 'Dest chains', title: 'Dest chains' },
+    { id: 'Contracts', title: 'Contracts' },
+    { id: 'Days', title: 'Days' },
+    { id: 'Weeks', title: 'Weeks' },
+    { id: 'Months', title: 'Months' },
+    { id: 'First tx', title: 'First tx' },
+    { id: 'Last tx', title: 'Last tx' },
+]
+
+const sourceNetworks = [
+    'ethereum',
+    'bsc',
+    'polygon',
+    'base',
+    'optimism',
+    'arbitrum',
+    'avalanche',
+    'zksync',
+    'fantom',
+    'coredao',
+    'gnosis',
+    'klaytn',
+    'harmony',
+    'celo',
+    'moonriver',
+    'moonbeam',
+    'dfk'
+]
